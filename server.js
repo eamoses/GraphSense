d24157cdb5e1df4b3fc18987a53a91e6f3b263de
eamoses <e.a.moses@gmail.com>
2016-05-28 10:03:45 -0700

    editing serverjs

--- server.js
+++ server.js
@@ -2,176 +2,57 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
+    logger = require('morgan'),
+    auth = require('./middleware/auth'),
+    controllers = require("./controllers");
 
-    //  NEW ADDITIONS
-    cookieParser = require('cookie-parser'),
-    session = require('express-session'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+// require and load dotenv
+require('dotenv').load();
 
 // configure bodyParser (for receiving form data)
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 
-// set view engine to hbs (handlebars)
-app.set('view engine', 'hbs');
+// log api requests
+app.use(logger('dev')); //middleware of logging in terminal each get/put etc
 
-// middleware for auth
-app.use(cookieParser());
-app.use(session({
-  secret: 'supersecretkey', // change this!
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+/*
+ * Auth Routes
+ */
 
-// require Graph model
-var db = require('./models'),
-    Graph = db.Graph;
-    User = db.User;
+var usersCtrl = controllers.users;
+app.post('/auth/signup', usersCtrl.signup);
+app.post('/auth/login', usersCtrl.login);
+app.get('/api/me', auth.ensureAuthenticated, usersCtrl.showCurrentUser); //checks wristband to pass to controller action, you can have as many as you want in the middle of the parenthasis
+app.put('/api/me', auth.ensureAuthenticated, usersCtrl.updateCurrentUser);
 
-// passport config
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+/*
+ * API Routes
+ */
+
+var postsCtrl = controllers.posts;
+app.get('/api/posts', postsCtrl.index);
+app.post('/api/posts', auth.ensureAuthenticated, postsCtrl.create);
+app.get('/api/posts/:id', postsCtrl.show);
+app.put('/api/posts/:id', auth.ensureAuthenticated, postsCtrl.update); //matching this update, making sure the user id is same as request, need ensureAuthenticated
+app.delete('/api/posts/:id', auth.ensureAuthenticated, postsCtrl.destroy);
 
 
-// HOMEPAGE ROUTE
-app.get('/', function (req, res) {
-    res.render('index', {user: JSON.stringify(req.user) + " || null"});
-});
-
-// API ROUTES
-
-// show signup view
-app.get('/signup', function (req, res) {
-  res.render('signup'); // you can also use res.sendFile
-});
-
-// sign up new user, then log them in
-// hashes and salts password, saves new user to db
-app.post('/signup', function (req, res) {
-  User.register(new User({ username: req.body.username }), req.body.password,
-    function (err, newUser) {
-      passport.authenticate('local')(req, res, function() {
-        res.redirect('/');
-      });
-    }
-  );
-});
-
-// show login view
-app.get('/login', function (req, res) {
-  res.render('login'); // you can also use res.sendFile
-});
-
-// log in user
-app.post('/login', passport.authenticate('local'), function (req, res) {
-  console.log(req.user);
-  res.redirect('/'); // preferred!
-});
-
-// log out user
-app.get('/logout', function (req, res) {
-  console.log("BEFORE logout", JSON.stringify(req.user));
-  req.logout();
-  console.log("AFTER logout", JSON.stringify(req.user));
-  res.redirect('/');
-});
-
-// get all graphs
-app.get('/api/graphs', function (req, res) {
-  // find all graphs in db
-  Graph.find(function (err, allGraphs) {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      res.json({ graphs: allGraphs });
-    }
-  });
-});
-
-// create new graph
-app.post('/api/graphs', function (req, res) {
-  // create new graph with form data (`req.body`)
-  var newGraph = new Graph(req.body);
-
-  // save new graph in db
-  newGraph.save(function (err, savedGraph) {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      res.json(savedGraph);
-    }
-  });
-});
-
-// get one graph
-app.get('/api/graphs/:id', function (req, res) {
-  // get graph id from url params (`req.params`)
-  var graphId = req.params.id;
-
-  // find graph in db by id
-  Graph.findOne({ _id: graphId }, function (err, foundGraph) {
-    if (err) {
-      if (err.name === "CastError") {
-        res.status(404).json({ error: "Nothing found by this ID." });
-      } else {
-        res.status(500).json({ error: err.message });
-      }
-    } else {
-      res.json(foundGraph);
-    }
-  });
-});
-
-// update graph
-app.put('/api/graphs/:id', function (req, res) {
-  // get graph id from url params (`req.params`)
-  var graphId = req.params.id;
-
-  // find graph in db by id
-  Graph.findOne({ _id: graphId }, function (err, foundGraph) {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      // update the gaphs's attributes
-      foundGraph.title = req.body.title;
-      foundGraph.description = req.body.description;
-
-      // save updated graph in db
-      foundGraph.save(function (err, savedGraph) {
-        if (err) {
-          res.status(500).json({ error: err.message });
-        } else {
-          res.json(savedGraph);
-        }
-      });
-    }
-  });
-});
-
-// delete graph
-app.delete('/api/graphs/:id', function (req, res) {
-  // get graph id from url params (`req.params`)
-  var graphId = req.params.id;
-
-  // find graph in db by id and remove
-  Graph.findOneAndRemove({ _id: graphId }, function (err, deletedGraph) {
-    if (err) {
-      res.status(500).json({ error: err.message });
-    } else {
-      res.json(deletedGraph);
-    }
-  });
+/*
+ * Catch All Route
+ */
+app.get(['/', '/signup', '/login', '/logout', '/profile', '/posts*'], function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// listen on port 3000
-app.listen(3000, function() {
-  console.log('server started');
+/*
+ * Listen on localhost:9000
+ */
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('server started on port ', port);
 });

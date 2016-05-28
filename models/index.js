0f225b646231769c88498ef2b335fd7e59325bae
eamoses <e.a.moses@gmail.com>
2016-05-28 12:21:33 -0700

    adding angular materialize

--- models/index.js
+++ models/index.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/graphSense' ||
                   process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL );
-
 module.exports = {
   User: require("./user"),
   Graph: require("./graph")

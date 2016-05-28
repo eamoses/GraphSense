3ac7dd1a5a995256b9c936d5769e935df7652eaa
eamoses <e.a.moses@gmail.com>
2016-05-28 09:58:28 -0700

    add bcrypt

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/microblog-app');
+mongoose.connect('mongodb://localhost/graphSense' ||
+                  process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL );
 
-module.exports.Graph = require("./graph");
-module.exports.User = require("./user");
+module.exports = {
+  User: require("./user"),
+  Graph: require("./graph")
+};

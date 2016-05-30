4ade9002a4672e8b95ec9c03e10d4826382ab34e
eamoses <e.a.moses@gmail.com>
2016-05-30 13:55:50 -0700

    pubnub added to angular

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,8 @@
 angular
   .module('GraphSense', [
     'ngRoute',
-    'satellizer'
+    'satellizer',
+    'pubnub.angular.service'
   ])
   .controller('MainController', MainController)
   .controller('GraphsIndexController', GraphsIndexController)
@@ -11,5 +12,15 @@ angular
   .controller('SignupController', SignupController)
   .controller('LogoutController', LogoutController)
   .controller('ProfileController', ProfileController)
+  .controller('ChatCtrl', function($scope, Pubnub) {
+   $scope.channel = 'test';
+   // Generating a random uuid between 1 and 100 using an utility function from the lodash library.
+   $scope.uuid = _.random(100);
+   Pubnub.init({
+     publish_key: 'pub-c-a3ff103c-13da-40f7-9134-45670c77ae43',
+     subscribe_key: 'sub-c-8740379a-1fcd-11e6-8b91-02ee2ddab7fe',
+         uuid: $scope.uuid
+       });
+  })
   .service('UserService', UserService)
   .config(configRoutes);
